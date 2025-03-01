584a33ba593234aee9d8846bcb4b59f72801f319
luc1k1 <153931924+luc1k1@users.noreply.github.com>
2025-03-01 20:03:24 +0400

    upd

--- users_manager.py
+++ users_manager.py
@@ -3,6 +3,7 @@ import bcrypt
 import string
 import random
 from tabulate import tabulate
+from datetime import datetime
 
 class Users:
     '''users manager'''
@@ -25,31 +26,36 @@ class Users:
             return {self.priority: {}} 
         except json.JSONDecodeError:     #except json.JSONDecodeError:
             print("❌ Error: File users.json is damaged! Fixing...")
-            return {self.priority: {}}  
+            return {self.priority: {}}
     
+    def log_action(self, action):
+        '''Log action'''
+        with open("logs.txt", "a") as file:
+            file.write(f"{datetime.now()}: {action}\n")
+
     def save_data(self): 
-        '''save data to json file'''
+        '''Save data to json file'''
         with open(self.filename, "w") as file:
             json.dump(self.data, file, indent=4)
     
 
     def hash_password(self, password):
-        '''hash password'''
+        '''Hash password'''
         self.password = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
         return self.password
 
     def verify_password(self, username, password):
-        ''''verify hashed password'''
+        ''''Verify hashed password'''
         return self.hash_password(password) == self.data[self.priority][username]
     
     
     
     def verify_name(self, username):
-        '''verify username'''
+        '''Verify username'''
         return username in self.data[self.priority]
 
     def verify_user(self, username, password):
-        '''verify users data'''
+        '''Verify users data'''
         if self.verify_name(username):
             self.data[self.priority][username] = self.hash_password(password)
             self.save_data()
@@ -60,22 +66,26 @@ class Users:
     
    
     def add_user(self, username, password):
-        '''add user'''
+        '''Add user'''
         if not self.verify_name(username):
             self.data[self.priority][username] = self.hash_password(password)
             self.save_data()
+            self.log_action(f"{username} registered")
             return True
         else:
             print("User already exists")
+            self.log_action(f"{username} failed to register")
             return False
     
     def login(self, username, password):
-        '''login'''
+        '''Login to account'''
         if self.verify_user(username, password):
             self.logged_in = username
             print(f"Welcome {username}")
+            self.log_action(f"{username} logged in")
             return True
         else:
+            self.log_action(f"{username} failed to login")
             return False
         
 
@@ -83,6 +93,7 @@ class Users:
     def logout(self):
         '''logout'''
         self.logged_in = False
+        self.log_action(f"{self.logged_in} logged out")
         return True
     
     def delete_user(self, username):
@@ -90,13 +101,16 @@ class Users:
         if self.logged_in == username:  #check if user is logged in and can delete account
             del self.data[self.priority][username]
             self.save_data()
+            self.log_action(f"{username} deleted their account")
             return True
         else:
+            self.log_action(f"{username} failed to delete their account")
             return "At first login,then you can delete your account"
 
     def user_exit(self):
         '''user exit'''
         self.logged_in = False #logout user
+        self.log_action(f"{self.logged_in} logged out")
         return True
     def generate_password(self, length = random.randint(8, 12)):
         '''generate password'''
@@ -118,14 +132,16 @@ class Admin(Users):
         if self.verify_user(username, password):
             self.__logged_in = username
             print(f"Welcome {username}")
+            self.log_action(f"{username} logged in")
             return True
         else:
             return False
 
     def change_user_password(self, username, password):
-        '''change user password  '''
+        '''Change user password for admin'''
         self.data[super().priority][username] = self.hash_password(password)
         self.save_data()
+        self.log_action(f"{username} changed their password")
         return True
     
 
@@ -134,6 +150,7 @@ class Admin(Users):
         if self.verify_name(username):
             del self.data[super().priority][username]
             self.save_data()
+            self.log_action(f"{username} deleted their account")
             return True
         else:
             return "User not found, please check the username"
@@ -162,12 +179,14 @@ class Admin(Users):
 
         format_table(users_data, "Users")
         format_table(admins_data, "Admins")
+        self.log_action("Users and Admins shown")
 
     def change_user_priority(self, username, priority):
         '''Change user priority'''      
         self.data[priority][username] = self.data[super().priority][username]
         del self.data[super().priority][username]
         self.save_data()
+        self.log_action(f"{username} changed their priority to {priority}")
         return True
         
         
